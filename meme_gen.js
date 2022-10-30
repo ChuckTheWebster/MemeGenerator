632debe1a41c3a859d7620349549c71d3ba19da0
Charlotte Webster <charlotte.shae.webster@gmail.com>
2022-10-30 13:42:38 -0600

    add variables img/text/submit/div for memes

---
+++ meme_gen.js
@@ -0,0 +1,9 @@
+const imgUrlInput = document.querySelector('#img-url-input');
+const textTopInput = document.querySelector('#text-top-input');
+const textBottomInput = document.querySelector('#text-bottom-input');
+const submit = document.querySelector('#submit');
+/* for potential use later:
+const textTopSize = document.querySelector('#text-top-size');
+const textBottomSize = document.querySelector('#text-bottom-size');
+*/
+const divForMemes = document.querySelector('#insert-memes-here');
